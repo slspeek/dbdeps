dcabb79ff32c4537f84477b24602722f257e7c91
Steven L. Speek <slspeek@gmail.com>
2020-04-09 01:13:12 +0200

    build.py cleaned up

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
-from pybuilder.core import use_plugin, init, task, depends, before, after
+from pybuilder.core import use_plugin, init, task, depends, description
 from zipfile import ZipFile
+from os.path import join
 import os
+import shutil
+
 
 use_plugin('python.core')
 use_plugin('python.unittest')
@@ -10,25 +13,36 @@ use_plugin('python.distutils')
 use_plugin('python.pydev')
 use_plugin('python.flake8')
 use_plugin('filter_resources')
+use_plugin('copy_resources')
 
 name = 'dbdeps'
 default_task = ['oxt']
-version = '0.1'
+version = '0.1.1'
+
 
 @init
 def set_properties(project):
     project.depends_on('graphviz')
     project.set_property('coverage_exceptions', ['main'])
     project.set_property('filter_resources_glob', ['**/description.xml'])
+    project.set_property("copy_resources_glob", ["META-INF/*",
+                                                 'description/*',
+                                                 'description.xml', 'icons/*.png',
+                                                 'AddonUI.xcu',
+                                                 'Accelerators.xcu',
+                                                 'LICENSE',
+                                                 'README.md',
+                                                 'src/main/python/**'])
+
+    project.set_property("copy_resources_target", "$dir_target/dbdeps_oxt")
     project.version = version
-    
-    
+
 
 def zipFilesInDir(dirName, zipFileName, root):
     # create a ZipFile object
     with ZipFile(zipFileName, 'w') as zipObj:
         # Iterate over all the files in directory
-        
+
         for folderName, subfolders, filenames in os.walk(dirName):
             for filename in filenames:
                 # create complete filepath of file in directory
@@ -37,65 +51,44 @@ def zipFilesInDir(dirName, zipFileName, root):
                 zipObj.write(filePath, os.path.relpath(filePath, root))
 
 
+def move(src, dst, logger):
+    r = shutil.move(src, dst)
+    logger.debug('moving {0} to {1} resulted in {2}'.format(src, dst, r))
+
+
 @task
 @depends('package')
+@description("Builds a LibreOffice extension file")
 def oxt(logger, project):
-    get = lambda p:project.expand_path(project.get_property(p))
-    join = os.path.join
-    
-    dir_dist = get('dir_target')
-    target = join(dir_dist, 'dbdeps_oxt')
-    dir_dist_dist = join(dir_dist, 'dist')
-    os.makedirs(dir_dist_dist, exist_ok=True)
-    file = join(dir_dist_dist, 'dbdeps.oxt')
-    
-    zipFilesInDir(target, file, target)
-    logger.info('LibreOffice extension file written')
 
+    dir_zip_source = project.expand_path('$dir_target/dbdeps_oxt')
+    dir_target_dist = project.expand_path('$dir_target/dist')
+    os.makedirs(dir_target_dist, exist_ok=True)
 
-def mkdir(logger, path):
-    logger.info("About to create: {0}".format(path))
-    os.makedirs(path, exist_ok=True)
-    
-@before('package')
-def prepare_oxt(logger, project):
-    logger.warn('prepare_oxt STARTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    get = lambda p:project.expand_path(project.get_property(p))
-    join = os.path.join
-    
-    dir_dist = get('dir_target')
-    mkdir(logger, dir_dist)
-    target = join(dir_dist, 'dbdeps_oxt')
-    mkdir(logger, target)
-    dir_python = join(target, 'python')
-    mkdir(logger, dir_python)
-    pythonpath = join(dir_python, 'pythonpath')
-    os.makedirs(pythonpath, exist_ok=True)
-    os.system('python -m pip install graphviz --target {0}'.format(pythonpath))
+    zip_file = join(dir_target_dist, 'dbdeps.oxt')
 
+    dir_pythonpath = project.expand_path(
+        '$dir_target/dbdeps_oxt/python/pythonpath')
+    if os.path.exists(dir_pythonpath):
+        shutil.rmtree(project.expand_path(
+        '$dir_target/dbdeps_oxt/python'))
+    os.makedirs(dir_pythonpath, exist_ok=True)
+    
+    os.system(
+        'python -m pip install graphviz --target {0}'.format(dir_pythonpath))
 
-    dir_src = get('dir_source_main_python')
-    from shutil import copytree, copy
-    c_target = join(pythonpath, 'dbdeps')
-    copytree(join(dir_src, 'dbdeps'), c_target)
-    def cp_to_target(file):
-        copy(join(dir_src, file), join(dir_python, file))
-    
-    cp_to_target('main.py')
-        
-    def cp_to_target(file):
-        copy(join(project.get_property('basedir'), file), join(target, file))
-    
-    def cptree_to_target(file):
-        copytree(join(project.get_property('basedir'), file), join(target, file))
-    
-    cp_to_target('description.xml')
-    cp_to_target('AddonUI.xcu')
-    cp_to_target('Accelerators.xcu')
-    cp_to_target('LICENSE')
-    cptree_to_target('META-INF')
-    cptree_to_target('description')
-    cptree_to_target('icons')
-    logger.warn('prepare_oxt ENDED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    
+    move(project.expand_path('$dir_target/dbdeps_oxt/src/main/python/dbdeps'),
+         project.expand_path(
+             '$dir_target/dbdeps_oxt/python/pythonpath'), logger
+         )
 
+    move(project.expand_path('$dir_target/dbdeps_oxt/src/main/python/main.py'),
+         project.expand_path(
+             '$dir_target/dbdeps_oxt/python/main.py'), logger
+         )
+
+    shutil.rmtree(project.expand_path('$dir_target/dbdeps_oxt/src/'))
+
+    zipFilesInDir(dir_zip_source, zip_file, dir_zip_source)
+
+    logger.info('LibreOffice extension file written to {0}'.format(zip_file))
